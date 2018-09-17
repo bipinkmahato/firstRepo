0d01821f16801903d8a898b52a588267cd62753c
Bipin Kumar Mahato <mahatobipin78@gmail.com>
2018-09-17 20:31:43 +0530

    Create number.java

---
+++ number.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+
+class Ideone
+{
+	public static void main (String[] args) {
+		Scanner sc = new Scanner(System.in);
+int n= sc.nextInt();
+if(n==0)
+System.out.println("zero");
+else if(n>0)
+System.out.println("positive");
+else 
+System.out.println("negative");
+}
+}
