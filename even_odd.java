e62ab42bbfe860a196660b2d91ace69505e74ef9
Bipin Kumar Mahato <mahatobipin78@gmail.com>
2018-09-17 21:53:38 +0530

    Create even_odd.java

---
+++ even_odd.java
@@ -0,0 +1,21 @@
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc= new Scanner(System.in);
+int n= sc.nextInt();
+if(n%2==0){
+System.out.println("even");
+} else if(n<0){
+System.out.println("Invalid");
+} else{
+ System.out.println("odd");
+}
+	}
+} 
