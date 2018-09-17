a3c76317a203e288445bbfc3301a1e830c8abe4f
Bipin Kumar Mahato <mahatobipin78@gmail.com>
2018-09-17 22:24:41 +0530

    Create vowel_consonant.java

---
+++ vowel_consonant.java
@@ -0,0 +1,20 @@
+
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc= new Scanner (System.in);
+char ch = sc.next().charAt(0);
+if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'||ch=='A'||ch=='E'||ch=='I'||ch=='O'||ch=='U'){
+System.out.println("vowel");
+} else {
+System.out.println("consonant");
+}
+	}
+}
